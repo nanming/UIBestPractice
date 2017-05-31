59af0a12ad8c3eabe677bb232fe9c4966f0055f5
unknown <>
2017-05-31 23:35:14 +0800

    First Init

---
+++ app/src/main/java/com/example/nanming/uibestpractice/MsgAdapter.java
@@ -0,0 +1,124 @@
+package com.example.nanming.uibestpractice;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import static com.example.nanming.uibestpractice.Msg.TYPE_RECEIVED;
+import static com.example.nanming.uibestpractice.Msg.TYPE_SEND;
+
+/**
+ * Created by NanMing on 2017/5/31 0031.
+ */
+
+public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
+
+    private List<Msg> mMsgList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout leftLayout;
+        LinearLayout rightLayout;
+
+        TextView msgLeft;
+        TextView msgRight;
+
+        public ViewHolder(View view) {
+            super(view);
+            leftLayout = (LinearLayout) view.findViewById(R.id.msg_layout_left);
+            rightLayout = (LinearLayout) view.findViewById(R.id.msg_layout_right);
+            msgLeft = (TextView) view.findViewById(R.id.msg_left);
+            msgRight = (TextView) view.findViewById(R.id.msg_right);
+        }
+    }
+
+    public MsgAdapter(List<Msg> msgList) {
+        mMsgList = msgList;
+    }
+
+    /**
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     * <p>
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     * <p>
+     * The new ViewHolder will be used to display items of the adapter using
+     * {@link #onBindViewHolder(ViewHolder, int, List)}. Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary {@link View#findViewById(int)} calls.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see #getItemViewType(int)
+     * @see #onBindViewHolder(ViewHolder, int)
+     */
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.msg_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method
+     * should update the contents of the {@link ViewHolder#itemView} to reflect the item at
+     * the given position.
+     * <p>
+     * Note that unlike {@link ListView}, RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the <code>position</code> parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use {@link ViewHolder#getAdapterPosition()} which will
+     * have the updated adapter position.
+     * <p>
+     * Partial bind vs full bind:
+     * <p>
+     * The payloads parameter is a merge list from {@link #notifyItemChanged(int, Object)} or
+     * {@link #notifyItemRangeChanged(int, int, Object)}.  If the payloads list is not empty,
+     * the ViewHolder is currently bound to old data and Adapter may run an efficient partial
+     * update using the payload info.  If the payload is empty,  Adapter must run a full bind.
+     * Adapter should not assume that the payload passed in notify methods will be received by
+     * onBindViewHolder().  For example when the view is not attached to the screen, the
+     * payload in notifyItemChange() will be simply dropped.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Msg msg = mMsgList.get(position);
+
+        if (msg.getType() == TYPE_RECEIVED) {
+            holder.leftLayout.setVisibility(View.VISIBLE);
+            holder.rightLayout.setVisibility(View.GONE);
+            holder.msgLeft.setText(msg.getContent());
+        } else if (msg.getType() == TYPE_SEND) {
+            holder.rightLayout.setVisibility(View.VISIBLE);
+            holder.leftLayout.setVisibility(View.GONE);
+            holder.msgRight.setText(msg.getContent());
+        }
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    @Override
+    public int getItemCount() {
+        return mMsgList.size();
+    }
+}
